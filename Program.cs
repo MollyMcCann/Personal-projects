64eff27853e10ea65ea67f6a8b2dc34b9b622d80
Molly McCann <56143091+MollyMcCann@users.noreply.github.com>
2020-03-30 14:58:32 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,44 @@
 ﻿using System;
-// okay so lets make an app that tracks emmett and evie (my dogs) age by establishing their birth year and then add
-//or subtract age depending on what year it is
-//tell user emmet and evies age this year (2020)
-//ask user to put in a year to see how old they will be
-//if they put in 2020 print age (do not add any thing)
-//if they go overthe age 16 print that is an old doggo
-namespace doggoAgeTracker
+
+namespace Practice_if_else
 {
     class Program
     {
         static void Main(string[] args)
         {
-           //int EmmettCurrentAge = 2;
-           // int EvieCurrentAge = 1;
-            //in 2020 Evie is 1 and Emmett is 2
-            Console.WriteLine("let's see how old my dogs are!");
-            Console.WriteLine();
-            Console.WriteLine("in 2020 Emmett is 2 and Evie is 1");
-            Console.WriteLine("Please enter a year:");
-            string year = Console.ReadLine();
-            int year2 = Int32.Parse(year);
-            int givenYear = year2 - 2020 +2;
-            int givenYear2 = year2 - 2020 + 1;
-            Console.WriteLine("Emmett's age is " + givenYear);
-            Console.WriteLine("Evie's age is " + givenYear2);
-            if (year2 > 2040)
+            // age warning for a game or site
+            Console.Write("Please enter your age: ");
+            Byte age = Byte.Parse(Console.ReadLine());
+            //we have just requested Users age now we will set some rules for the age of User allowed to view content
+            if (age < 14)
             {
-                Console.WriteLine("that's an old Doggo");
-            } 
-            if (year2 < 2018)
-            {
-                Console.WriteLine( "they weren't born yet!");
+                Console.WriteLine("Users under 14 must have a parents permission");
             }
-           
-            
-            Console.ReadLine();
+            else
+            {
+                Console.WriteLine("Enjoy your Game!");
+            }
+            Console.Write("please enter your first number : ");
+            int firstNumbr = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your second number : ");
+            int secoundNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the sum of {0} and  {1} : ",
+                firstNumbr,secoundNumber);
+            int userTotal = Int32.Parse(Console.ReadLine());
+
+            int actualTotal = firstNumbr + secoundNumber;
+            if (actualTotal == userTotal)
+            {
+                Console.WriteLine("You are correct!");
+            }
+            else
+            {
+                Console.WriteLine("that is incorrect, the correct total is  {0}",actualTotal);
+            }
+
+           // Console.WriteLine("10>15: {0}", 10 > 15) ; 
+            Console.ReadKey();
+
         }
     }
 }
